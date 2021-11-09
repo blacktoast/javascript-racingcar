07c4729a228cf008e78d2b0640893778728527a7
blacktoast <dbstjd100@naver.com>
2021-11-09 20:36:10 +0900

    feat : 이벤트 컨트롤러 파일분리

---
+++ src/js/controller/carEvent.js
@@ -0,0 +1,37 @@
+import { renderCar } from "../render/renderCarName.js";
+import { getCarNames } from "../utils/dom.js";
+
+function checkEmpty(input) {
+  if (input.includes("")) {
+    alert("공백을 입력하지 마세요");
+    return false;
+  }
+  return true;
+}
+
+function checkInputLength(input) {
+  input = input.filter((e) => {
+    return e.length > 5;
+  });
+  console.log(input);
+  if (input.length !== 0) {
+    alert("차 이름은 5글자를 넘을수가 없습니다.");
+    return false;
+  }
+  return true;
+}
+
+function checkCarName() {
+  let names = getCarNames();
+  console.log(names);
+  if (checkEmpty(names) && checkInputLength(names)) {
+    return true;
+  }
+  return false;
+}
+
+export function addPutCarNameEvent({ target }) {
+  if (checkCarName()) {
+    renderCar();
+  }
+}
--- src/js/controller/initEvents.js
+++ src/js/controller/initEvents.js
@@ -5,47 +5,15 @@ import {
   getCarContainer,
   getCarNames,
 } from "./../utils/dom.js";
+import { addPutCarNameEvent } from "./carEvent.js";
 
 function addRacingEvent({ target }) {
   console.log(target);
   let carNames = getCarNames();
   let retryNumber = getAttempts();
-  console.log(carNames, retryNumber);
+  //r게임 시작
 }
 
-function checkEmpty(input) {
-  if (input.includes("")) {
-    alert("공백을 입력하지 마세요");
-    return false;
-  }
-  return true;
-}
-function checkInputLength(input) {
-  input = input.filter((e) => {
-    return e.length > 5;
-  });
-  console.log(input);
-  if (input.length !== 0) {
-    alert("차 이름은 5글자를 넘을수가 없습니다.");
-    return false;
-  }
-  return true;
-}
-
-function checkCarName() {
-  let names = getCarNames();
-  console.log(names);
-  if (checkEmpty(names) && checkInputLength(names)) {
-    return true;
-  }
-  return false;
-}
-
-function addPutCarNameEvent({ target }) {
-  if (checkCarName()) {
-    renderCar();
-  }
-}
 export function initEvents() {
   let $putCar = $(".put-car-name");
   console.log($putCar);
